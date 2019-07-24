33efc8117a8f80ca73326f68f5e185fcd4d3d8de
krissanawat101 <krissanawat101@gmail.com>
2019-07-24 07:26:25 +0700

    fixing some issue as adam mentioned

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,4 @@
 export default (config = {
-  pubnub_publishKey: "pub-c-1c3e0e3a-b8f4-4889-bae3-c11e4b1a98c5",
-  pubnub_subscribeKey: "sub-c-6b4eb3f4-9f13-11e9-8fba-5e9db976464b",
-  onesignal_key: "477a0ca6-d919-4e8b-a14a-46d43182a96b"
+  pubnub_publishKey: "pub-c-b7c90e2f-60cf-4a1f-968e-612830e0378e",
+  pubnub_subscribeKey: "sub-c-90bd28b2-aaaa-11e9-a87a-b2acb6d6da6e"
 });
